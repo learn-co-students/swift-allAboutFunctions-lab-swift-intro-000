3cdee0a9a37d4248c53be4f358f108df68cdd436
Nuruddin <nuruddin.iminohunov@gmail.com>
2017-05-03 15:52:52 -0500

    Final

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,9 @@
  */
 // write your code here
 
-
+func frozen(){
+    print("Let it go!")
+}
 
 
 
@@ -26,6 +28,10 @@
  ### 2. Write a function named `frozenAgain` that takes no arguments. Declare a constant in the body of the function, and assign it the value "Let it go!". Then print it to the console.
  */
 // write your code here
+func frozenAgain(){
+    let constant = "Let it go!"
+    print(constant)
+}
 
 
 
@@ -38,7 +44,9 @@
  */
 // write your code here
 
-
+func frozenCharacter(name: String){
+    print("My favorite character is \(name) ")
+}
 
 
 
@@ -49,7 +57,13 @@
  */
 // write your code here
 
-
+let characterOne = "SuperMan"
+var characterTwo = "Batman"
+frozenCharacter(name: characterOne)
+frozenCharacter(name: characterTwo)
+characterTwo = "Shrek"
+//characterOne = "Not Changed"
+frozenCharacter(name: characterTwo)
 
 
 
@@ -61,8 +75,10 @@
 // write your code here
 
 
-
-
+func haveProblems(numberOfProblems: Int){
+    print("I got \(numberOfProblems) but Swift ain't one ")
+}
+haveProblems(numberOfProblems: 200)
 
 
 
@@ -72,19 +88,22 @@
  */
 // write your code here
 
+func bandPrinter(nameOfband: String, number: Int){
+    print("\(number) favorite band is \(nameOfband)")
+}
 
-
+bandPrinter(nameOfband: "Bojalar", number: 1)
 
 
 
 /*: question7
  ### 7. The code below is broken. Can you identify which line has an error and fix it so that it works again? Uncomment the code below before starting.
  */
-//func badFavoriteBand(bandName: String, position: Int) {
-//    print("My #\(position) favorite band is \(bandName).")
-//}
-//
-//badFavoriteBand("The Beatles", 2)
+func badFavoriteBand(bandName: String, position: Int) {
+    print("My #\(position) favorite band is \(bandName).")
+}
+
+badFavoriteBand(bandName: "The Beatles", position: 2)
 
 
 
@@ -95,11 +114,11 @@
 /*: question8
  ### 8. This code is broken, too. Assume the call to the function is correct. What's broken about the function definition? Can you fix it? Uncomment the code below before starting.
  */
-//func alsoBadFavoriteBand(bandName: String, position: String) {
-//    print("My #\(position) favorite band is \(bandName)")
-//}
-//
-//alsoBadFavoriteBand(bandName: "Blink-182", position: 42)
+func alsoBadFavoriteBand(bandName: String, position: String) {
+    print("My #\(position) favorite band is \(bandName)")
+}
+
+alsoBadFavoriteBand(bandName: "Blink-182", position: "42")
 
 
 
@@ -112,8 +131,10 @@
  */
 // write your code here
 
-
-
+func madLib(characterName: String, noun: String, preposition: String){
+    print("To \(noun) and \(preposition), \(characterName)!")
+}
+madLib(characterName: "BatMan", noun: "Any Noun", preposition: "Any preposition")
 
 
 
@@ -123,7 +144,9 @@
  */
 // write your code here
 
-
+func buzzReturn() -> String{
+    return "Buzz Lightyear to the rescue"
+}
 
 
 
@@ -133,7 +156,9 @@
  ### 11. Create a function that takes no arguments and returns any number.
  */
 // write your code here
-
+func someFunc() -> Int{
+    return 8
+}
 
 
 
@@ -143,9 +168,11 @@
  ### 12. Create a function that takes in a characters name. This function will return back a `String` as follows: "To infinity and beyond, <character name>!". The character name should be returned uppercased.
  */
 // write your code here
+func stringer(name: String) -> String {
+    return "To infinity and beyond, \(name.uppercased())"
+}
 
-
-
+print(stringer(name: "Hey"))
 
 
 
