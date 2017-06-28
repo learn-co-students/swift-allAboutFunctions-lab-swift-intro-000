f0dc6062fbf6cfc3f3845ccc94f09c0e923cea61
hirofudeuchi <hiro.fudeuchi@gmail.com>
2017-06-28 15:26:12 +0100

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,31 +13,33 @@
 /*: question1
  ### 1. Create a function named `frozen` which takes no arguments. When this function is called, it prints "Let it go!".
  */
-// write your code here
+func frozen(){
+    print("Let it go!")
+}
 
-
-
-
-
+frozen()
 
 
 
 /*: question2
  ### 2. Write a function named `frozenAgain` that takes no arguments. Declare a constant in the body of the function, and assign it the value "Let it go!". Then print it to the console.
  */
-// write your code here
+func frozenAgain(){
+    let verse = "Let it go!"
+    print(verse)
+}
 
-
-
-
-
+frozenAgain()
 
 
 /*: question3
  ### 3. Write a function that takes in a character's name as an argument (it can be any character from anything). What should the type of that argument be? Print the message "My favorite character is <character name>." to the screen.
  */
-// write your code here
+func favouriteCharacter(name: String){
+    print("My favourite character is \(name).")
+}
 
+favouriteCharacter(name: "Rory")
 
 
 
@@ -47,21 +49,27 @@
 /*: question4
  ### 4. Call the function you wrote in Question 3 using a constant you define. Then call it using a variable. Change the value of the variable, and call it again. What do you see in the console?
  */
-// write your code here
+let favorite = "Michel"
+favouriteCharacter(name: favorite)
 
+var favoriteFriend: String = "Chandler"
+favouriteCharacter(name: "Alan")
 
+favoriteFriend = "Joey"
+favouriteCharacter(name: favoriteFriend)
 
-
-
-
+var x = 12
+x = 14
 
 /*: question5
  ### 5. Write a function that takes an integer as an argument and prints the string "I got <number> problems but Swift ain't one" to the console.
  */
 // write your code here
+func problems(number: Int){
+    print("I got \(number) problems but Swift ain't one")
+}
 
-
-
+problems(number: 12)
 
 
 
@@ -71,48 +79,43 @@
  ### 6. Write a function that takes two arguments, the name of a band (a `String`) and a number (an `Int`). It should print the message "My #<number> favorite band is <band>." to the console.
  */
 // write your code here
+func bandRank(name: String, rank: Int){
+    print("My #\(rank) favorite band is \(name).")
+}
 
-
-
+bandRank(name: "Foo Fighters", rank: 2)
 
 
 
 /*: question7
  ### 7. The code below is broken. Can you identify which line has an error and fix it so that it works again? Uncomment the code below before starting.
  */
-//func badFavoriteBand(bandName: String, position: Int) {
-//    print("My #\(position) favorite band is \(bandName).")
-//}
-//
-//badFavoriteBand("The Beatles", 2)
+func badFavoriteBand(bandName: String, position: Int) {
+    print("My #\(position) favorite band is \(bandName).")
+}
 
-
-
-
-
+badFavoriteBand(bandName: "The Beatles", position: 2)
 
 
 /*: question8
  ### 8. This code is broken, too. Assume the call to the function is correct. What's broken about the function definition? Can you fix it? Uncomment the code below before starting.
  */
-//func alsoBadFavoriteBand(bandName: String, position: String) {
-//    print("My #\(position) favorite band is \(bandName)")
-//}
-//
-//alsoBadFavoriteBand(bandName: "Blink-182", position: 42)
+func alsoBadFavoriteBand(bandName: String, position: Int) {
+    print("My #\(position) favorite band is \(bandName)")
+}
 
-
-
-
+alsoBadFavoriteBand(bandName: "Blink-182", position: 42)
 
 
 
 /*: question9
  ### 9. Let's play Mad Libs! Create a function called `madLib`. It should take three parameters: A character name, a noun, and a preposition, and print out the line "To <noun> and <preposition>, <character name>!" to the console. Don't forget to call your function to test it out!
  */
-// write your code here
+func madLib(name: String, noun: String, preposition: String){
+    print("To \(noun) and \(preposition), \(name)!")
+}
 
-
+madLib(name: "Woody", noun: "Infinity", preposition: "Beyond")
 
 
 
@@ -121,10 +124,9 @@
 /*: question10
  ### 10. Create a function that takes no arguments and returns the string "Buzz Lightyear to the rescue!"
  */
-// write your code here
-
-
-
+func buzz() -> String {
+    return "Buzz Lightyear to the rescue!"
+}
 
 
 
@@ -132,7 +134,9 @@
 /*: question11
  ### 11. Create a function that takes no arguments and returns any number.
  */
-// write your code here
+func foo() -> Int {
+    return 42
+}
 
 
 
@@ -142,12 +146,11 @@
 /*: question12
  ### 12. Create a function that takes in a characters name. This function will return back a `String` as follows: "To infinity and beyond, <character name>!". The character name should be returned uppercased.
  */
-// write your code here
+func bar(name: String) -> String {
+    return "To infinity and beyond, \(name.uppercased()!)"
+}
 
-
-
-
-
+print(bar(name: "Buzz"))
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-allAboutFunctions-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
 
