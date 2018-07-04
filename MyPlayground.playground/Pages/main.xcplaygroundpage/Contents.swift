8bd238775052629cc1bcad59b007da3dbdcd41e0
Martin <murgiles@me.com>
2018-07-04 01:44:07 -0300

    Complete problems

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -8,63 +8,61 @@
  * [Functions](https://github.com/learn-co-curriculum/swift-functions-readme)
  * [Functions with multiple arguments](https://github.com/learn-co-curriculum/swift-funcMultipleArg-readme)
  * [Functions with return values](https://github.com/learn-co-curriculum/swift-functionReturn-readme)
-
+ 
  */
 /*: question1
  ### 1. Create a function named `frozen` which takes no arguments. When this function is called, it prints "Let it go!".
  */
 // write your code here
 
-
-
-
-
-
-
+func frozen(){
+    print("Let it go!")
+}
+frozen()
 
 /*: question2
  ### 2. Write a function named `frozenAgain` that takes no arguments. Declare a constant in the body of the function, and assign it the value "Let it go!". Then print it to the console.
  */
 // write your code here
 
-
-
-
-
-
+func frozenAgain(){
+    let msj="Let it go!"
+    print(msj)
+}
+frozen()
 
 /*: question3
  ### 3. Write a function that takes in a character's name as an argument (it can be any character from anything). What should the type of that argument be? Print the message "My favorite character is <character name>." to the screen.
  */
 // write your code here
 
+func fav(_ char:String){
+    print("My favorite character is \(char)")
+}
 
-
-
-
-
+fav("Eleven")
 
 /*: question4
  ### 4. Call the function you wrote in Question 3 using a constant you define. Then call it using a variable. Change the value of the variable, and call it again. What do you see in the console?
  */
 // write your code here
 
-
-
-
-
-
-
+let fav1="Eleven"
+fav(fav1)
+var fav2="Dustin"
+fav(fav2)
+fav2="Mike"
+fav(fav2)
 /*: question5
  ### 5. Write a function that takes an integer as an argument and prints the string "I got <number> problems but Swift ain't one" to the console.
  */
 // write your code here
 
+func prob(_ number:Int){
+    print("I got \(number) problems but Swift ain't one")
+}
 
-
-
-
-
+prob(2)
 
 
 /*: question6
@@ -72,19 +70,21 @@
  */
 // write your code here
 
+func band(_ name:String,_ number:Int){
+    print("My #\(number) favorite band is \(name).")
+}
 
-
-
+band("Pink Floyd",1)
 
 
 /*: question7
  ### 7. The code below is broken. Can you identify which line has an error and fix it so that it works again? Uncomment the code below before starting.
  */
-//func badFavoriteBand(bandName: String, position: Int) {
-//    print("My #\(position) favorite band is \(bandName).")
-//}
-//
-//badFavoriteBand("The Beatles", 2)
+func badFavoriteBand(_ bandName:String,_ position: Int) {
+    print("My #\(position) favorite band is \(bandName).")
+}
+
+badFavoriteBand("The Beatles", 2)
 
 
 
@@ -95,48 +95,41 @@
 /*: question8
  ### 8. This code is broken, too. Assume the call to the function is correct. What's broken about the function definition? Can you fix it? Uncomment the code below before starting.
  */
-//func alsoBadFavoriteBand(bandName: String, position: String) {
-//    print("My #\(position) favorite band is \(bandName)")
-//}
-//
-//alsoBadFavoriteBand(bandName: "Blink-182", position: 42)
+func alsoBadFavoriteBand(bandName:String, position:Int) {
+    print("My #\(position) favorite band is \(bandName)")
+}
 
-
-
-
-
-
-
+alsoBadFavoriteBand(bandName: "Blink-182", position: 42)
 /*: question9
  ### 9. Let's play Mad Libs! Create a function called `madLib`. It should take three parameters: A character name, a noun, and a preposition, and print out the line "To <noun> and <preposition>, <character name>!" to the console. Don't forget to call your function to test it out!
  */
 // write your code here
 
+func madLib(_ charname:String,_ noun:String,_ prep:String){
+    print("To \(noun) and \(prep), \(charname)!")
+}
 
-
-
-
-
-
+madLib("Buzz","infinity","beyond")
 /*: question10
  ### 10. Create a function that takes no arguments and returns the string "Buzz Lightyear to the rescue!"
  */
 // write your code here
 
+func resc()->String{
+    return "Buzz Lightyear to the rescue!"
+}
 
-
-
-
-
-
+resc()
 /*: question11
  ### 11. Create a function that takes no arguments and returns any number.
  */
 // write your code here
 
+func ret()->Int{
+    return 5
+}
 
-
-
+ret()
 
 
 /*: question12
@@ -144,10 +137,13 @@
  */
 // write your code here
 
+func upp(_ name:String)->String{
+    return "To infinity and beyond, \(name.uppercased())!"
+}
 
-
-
+upp("Buzz")
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-allAboutFunctions-lab/blob/solution/MyPlayground.playground/Pages/solution.xcplaygroundpage/Contents.swift) for a link to the solution.
+
 
